c0807b03fedff4cb51f9696db5df66eb78f7d20b
Deepak Sharma <sharma.jnu@gmail.com>
2018-03-22 20:37:50 +0530

    Added new wizard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WizardComponent } from './wizard/wizard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WizardComponent
   ],
   imports: [
     BrowserModule
